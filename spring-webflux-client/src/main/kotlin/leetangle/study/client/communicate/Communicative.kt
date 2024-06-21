7471d8c578ffbd47819ca60db445690571826d81
sm <smlee@danal.co.kr>
2024-06-21 12:47:04 +0900

    타 프로젝트자료 정리

--- spring-webflux-client/src/main/kotlin/leetangle/study/client/communicate/Communicative.kt
+++
@@ -1,8 +0,0 @@
-package leetangle.study.client.communicate
-
-import reactor.core.publisher.Mono
-
-interface Communicative<R, S> {
-    fun requestBlock(request: R) : S
-    fun requestNonBlock(request: R) : Mono<S>
-}
--- spring-webflux-client/src/main/kotlin/leetangle/study/crypto/CryptoStrategy.kt
+++
@@ -1,6 +0,0 @@
-package leetangle.study.crypto
-
-interface CryptoStrategy {
-    fun encrypt(data: String): String
-    fun decrypt(data: String): String
-}
